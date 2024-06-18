0e9d9a0e993f9be483095a38b69296c5a9f73ee2
YudithOcto <yudith.octo@gmail.com>
2024-06-18 12:50:31 +0700

    complete the screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,89 @@
-import Image from "next/image";
+// pages/index.tsx
 
-export default function Home() {
+import Head from 'next/head';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
+      <Head>
+        <title>Grooshack</title>
+        <meta name="description" content="Welcome to Grooshack" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <header className="bg-white text-white p-4 flex flex-row justify-between animate-fade-in-down">
+        <div className="flex flex-col md:flex-row">
+          <h1 className="text-2xl font-bold text-sky-800">Grooshack</h1>
+          <div className="hidden md:block w-auto md:ml-4">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="border border-gray-300 rounded-md px-4 py-1 pr-10"
+            />
+          </div>
+        </div>
+        <div className='flex flex-row items-center justify-end w-fit'>
+          <p className='text-neutral-950 text-sm font-medium cursor-pointer hover:underline mr-4'>Log In</p>
+          <button className="bg-sky-800 px-4 py-2 rounded-md md:ml-4">Sign Up</button>
+        </div>
+      </header>
+      <div className='px-8 bg-white md:hidden'>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="bg-white w-full border border-gray-300 p-2 rounded-md"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <main className="flex-grow px-8 md:px-48 bg-white animate-fade-in-down">
+        <h2 className="text-3xl font-bold mb-4 md:mt-20 mt-8">Welcome to Grooshack</h2>
+        <p className='text-base w-full md:w-4/5 lg:w-1/2'>
+          Whatever your interest, from hiking and reading to networking and skill sharing, there are thousands of people who share it on Meetup. Events are happening every day—sign up to join the fun.
+        </p>
+        <button className="bg-sky-800 p-2 mt-4 text-white text-sm rounded-md">Join Grooshack</button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="w-full md:w-10/12 my-20">
+          <div className="flex justify-between items-center mb-4">
+            <h3 className="text-2xl font-bold">Upcoming Events</h3>
+            <a href="#" className="text-blue-500 text-sm font-semibold">See All Events</a>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+            {[1, 2, 3, 4].map((event, index) => (
+              <div key={index} className="border rounded-lg p-4 animate-fade-in-up">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM3qQ1bNASQVaZlJBH7zjmTMnoBVoHo_rMzg&s" alt="Event Image" className="w-full h-32 object-cover rounded-md mb-4" />
+                <h4 className="text-xl font-bold mb-2">Event Title {index + 1}</h4>
+                <p className="text-gray-700 mb-2">Hosted by: Host Name</p>
+                <p className="text-gray-500 mb-2 font-light text-sm">Mon, June 17, 14:00 WIB</p>
+                <p className={`text-sm font-bold ${index % 2 === 0 ? 'text-green-500' : 'text-red-500'}`}>
+                  {index % 2 === 0 ? 'Free' : 'Paid'}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="p-4 w-full bg-sky-50 mt-16 md:p-8 md:flex md:flex-row hidden items-center justify-between animate-fade-in-bottom">
+            <div className="md:w-1/2">
+              <h2 className="text-3xl font-bold mb-4">Join Grooshack</h2>
+              <p className="text-sm lg:w-10/12">
+                People use Meetup to meet new people, learn new things, find support, get out of their comfort zones, and pursue their passions, together. Membership is free.
+              </p>
+              <button className="bg-sky-800 p-2 mt-4 text-white text-sm rounded-md">Join Grooshack</button>
+            </div>
+            <img
+              src="https://www.meetup.com/_next/image/?url=%2Fimages%2FindexPage%2Fjoin%2Fjoin_meetup.webp&w=750&q=75"
+              alt="join_image"
+              className="md:h-32 object-cover rounded-md mb-4 md:mb-0 md:ml-4"
+            />
+          </div>
+        </div>
+      </main>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="bg-gray-800 text-white p-4 text-center animate-fade-in-up">
+        © 2024 Grooshack. All rights reserved.
+      </footer>
+    </div>
   );
-}
+};
+
+export default Home;
